6cc8481864e140ca0bc3cffef806c07254140927
yetanothercheer <yetanothercheer@protonmail.com>
2020-10-17 10:16:15 +0800

    Polish MainChoice view

--- app/src/main/java/dev/paranoid/MainActivity.kt
+++ app/src/main/java/dev/paranoid/MainActivity.kt
@@ -1,15 +1,16 @@
 package dev.paranoid
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
+import android.graphics.drawable.ScaleDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
 import android.widget.*
+import androidx.core.graphics.set
 import androidx.core.view.children
 import androidx.core.view.get
+import androidx.core.view.marginLeft
 
 class TabLayout(c: Context): LinearLayout(c) {
     lateinit var tabs: LinearLayout
@@ -19,8 +20,9 @@ class TabLayout(c: Context): LinearLayout(c) {
         orientation = LinearLayout.VERTICAL
 
         addView(LinearLayout(context).apply {
+            setPadding(40, 20, 40, 20)
             tabs = this
-        }, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT))
+        }, LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 150))
     }
 
     fun add(tab: View, v: View) {
@@ -40,6 +42,22 @@ class TabLayout(c: Context): LinearLayout(c) {
 class ChoiceView(c: Context): androidx.appcompat.widget.AppCompatTextView(c) {
     val size = 5;
     var index = 0;
+    val colors = listOf<Int>(0xFFF0022, 0xF000000, 0xF00FF00, 0xF0022FF, 0xFFF2200)
+
+    lateinit var bitmap: Bitmap
+
+    init {
+        bitmap = Bitmap.createBitmap(1080 - 20, 1700, Bitmap.Config.ARGB_8888)
+        freshBitmap()
+    }
+
+    fun freshBitmap() {
+        for (i in 0..(bitmap.width - 1)) {
+            for (j in 0..(bitmap.height - 1)) {
+                bitmap[i, j] = colors[index]
+            }
+        }
+    }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if (event != null) {
@@ -50,18 +68,19 @@ class ChoiceView(c: Context): androidx.appcompat.widget.AppCompatTextView(c) {
             }
         }
         index %= size
+        freshBitmap()
         invalidate()
         return super.onTouchEvent(event)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        text = "Image %d".format(index + 1)
         canvas?.apply {
             drawLine(10F + (width - 20F) * index / size, 10F, 10F + (width - 20F) * (index + 1) / size, 10F, Paint().apply {
                 color = Color.GREEN
                 strokeWidth = 10F
             })
+            drawBitmap(bitmap, 10F, 20F, Paint().apply {  })
         }
     }
 }
@@ -73,6 +92,7 @@ class MainChoice(c: Context): LinearLayout(c) {
         addView(LinearLayout(c).apply {
             addView(Button(c).apply { text = "no" })
             addView(Button(c).apply { text = "yes" })
+            setPadding(290, 0, 0, 30)
         }, LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT))
     }
 }
@@ -107,10 +127,47 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(TabLayout(this).apply {
-            add(TextView(context).apply { text = "MainChoice" }, MainChoice(context))
-            add(TextView(context).apply { text = "YouWereChoosen" }, YouWereChoosen(context))
-            add(TextView(context).apply { text = "Chat" }, Chat(context))
-            add(TextView(context).apply { text = "Profile" }, Profile(context))
+            add(ImageButton(context).apply {
+                setImageDrawable(
+                    resources.getDrawable(R.drawable.ic_launcher_background).apply {
+                        scaleX = 0.5F
+                        scaleY = 0.5F
+                    }
+                )
+            }, MainChoice(context))
+            add(ImageButton(context).apply {
+                setImageDrawable(
+                    resources.getDrawable(R.drawable.ic_launcher_background).apply {
+                        scaleX = 0.5F
+                        scaleY = 0.5F
+                    }
+                )
+            }, YouWereChoosen(context))
+            add(ImageButton(context).apply {
+                setImageDrawable(
+                    resources.getDrawable(R.drawable.ic_launcher_background).apply {
+                        scaleX = 0.5F
+                        scaleY = 0.5F
+                    }
+                )
+            }, Chat(context))
+            add(ImageButton(context).apply {
+                setImageDrawable(
+                    resources.getDrawable(R.drawable.ic_launcher_background).apply {
+                        scaleX = 0.5F
+                        scaleY = 0.5F
+                    }
+                )
+            }, Profile(context))
+        })
+
+        (window.decorView as FrameLayout).addView(TextView(this).apply {
+            text = "DecorView Test"
+            setTextColor(Color.BLUE)
+        },
+            FrameLayout.LayoutParams(FrameLayout.LayoutParams.WRAP_CONTENT, FrameLayout.LayoutParams.WRAP_CONTENT).apply {
+            leftMargin = 700
+            topMargin = 700
         })
     }
 }
