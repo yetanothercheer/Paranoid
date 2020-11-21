28a06c06327e4e5c19e6d7700f00bb0e9e4c6f13
yetanothercheer <yetanothercheer@protonmail.com>
2020-11-21 17:38:05 +0800

    Add fuel http library

--- app/src/main/java/dev/paranoid/ViewModel.kt
+++ app/src/main/java/dev/paranoid/ViewModel.kt
@@ -1,8 +1,14 @@
 package dev.paranoid
 
+import android.app.Application
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.gson.responseObject
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 data class Profile(
     val name: String,
@@ -20,14 +26,21 @@ fun gen(): Profile {
     return Profile(name, desc, (1..imageLen).map { "" })
 }
 
+data class TinderProfile(var id: String, var name: String, var about: String, var avatars: List<String>, var likes: List<String>)
 
 class MyModel: ViewModel() {
     var profile: MutableLiveData<Profile> = MutableLiveData()
 
     init {
+        GlobalScope.launch {
+            Fuel.post("https://paranoid.yetanothercheer.vercel.app/api", listOf(Pair("f", "register"))).responseObject<TinderProfile> { _, _, result ->
+                if (result != null) {
+                    // do something...
+                }
+            }
+        }
         profile.value = Profile("Cheer", "Yet another cheer", listOf("1", "2", "3"))
     }
-
 
     fun like() {
         profile.value = gen()
